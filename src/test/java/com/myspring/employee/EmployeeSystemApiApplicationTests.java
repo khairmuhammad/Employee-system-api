c557aed08c9d3531827251303981bf8620ad39e0
Khair <khairmuhammadmemon@healthhub.kr>
2022-03-30 15:35:11 +0500

    springboot backend

---
+++ src/test/java/com/myspring/employee/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspring.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
